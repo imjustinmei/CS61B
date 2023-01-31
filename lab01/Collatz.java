068b88e26660873fb9f653e076e0c5ab7f17d791
Justin Mei <imjustinmei@gmail.com>
2023-01-30 20:19:18 -0500

    revert to older commit

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -5,13 +5,10 @@ public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        // TODO: Fill in this method.
+        if (n == 1) return n;
+        if (n % 2 == 0) return n / 2;
+        else return 3 * n + 1;
     }
 
     public static void main(String[] args) {
