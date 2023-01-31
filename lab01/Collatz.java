853e2ea4b18d5aff1bb2d4e5d02aa7b63dd5eb2d
Justin Mei <imjustinmei@gmail.com>
2023-01-30 19:59:40 -0500

    unfix method

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -5,10 +5,13 @@ public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        // TODO: Fill in this method.
-        if (n == 1) return n;
-        if (n % 2 == 0) return n / 2;
-        else return 3 * n + 1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
